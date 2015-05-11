79141f2c15ff4a7e06eff05753e4b73fc1a5e8a3
nikhartman <nik.hartman@gmail.com>
2015-05-11 17:30:39 -0400

    add rc file

---
+++ mplrc.py
@@ -0,0 +1,47 @@
+# Stolen from Dan Allan. Edited some after that.
+ 
+# Plot area
+figure.figsize: 10, 8
+figure.facecolor: white
+figure.edgecolor: white
+axes.facecolor : white
+axes.grid: False
+ 
+# Axes and ticks
+axes.linewidth: 2
+axes.edgecolor: black
+xtick.direction: in
+ytick.direction: in
+xtick.major.size: 10
+xtick.major.width: 2
+xtick.minor.size: 10
+xtick.minor.width: 2
+ytick.major.size: 10
+ytick.major.width: 2
+ytick.minor.size: 10
+ytick.minor.width: 2
+ 
+# Space xlabels lower than default.
+xtick.major.pad: 20
+xtick.minor.pad: 20
+ 
+# Fonts and font sizes for various elements.
+font.family: serif
+font.serif: Computer Modern
+text.usetex: True  # All text will be in the LaTeX font.
+font.size: 20
+axes.titlesize: 20
+axes.labelsize: 15
+xtick.labelsize: 20
+ytick.labelsize: 20
+legend.fontsize: 12
+ 
+image.cmap: gray
+ 
+# Plot elements
+lines.linewidth: 5
+lines.markersize: 18
+ 
+# savefig options (avoid clipping at margins)
+savefig.bbox : tight
+savefig.pad_inches : 0.2
